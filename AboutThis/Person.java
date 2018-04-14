488085a7eec111824cd459beb11f6efe4c49f6d0
Sandra Opone <31860053+Sandraopone1@users.noreply.github.com>
2018-04-14 14:15:00 -0400

    Add files via upload

---
+++ AboutThis/Person.java
@@ -0,0 +1,29 @@
+
+// public class Person {
+//     private int age;
+//     private int cmHeight;
+//     private String name;
+//     public Person() {
+//         this(20, "John Doe", 171);
+//     }
+    
+//     public Person(int age, String name, int cmHeight) {
+//         this.age = age;
+//         this.name = name;
+//         this.cmHeight = cmHeight;
+//     }
+//     // ...
+
+class Person {
+    private int age;
+    private String name;
+    public Person(int ageParam, String nameParam) {
+        this.age = ageParam;
+        this.name = nameParam;
+    }
+    public void objectMethods(Person anotherObject) {
+        System.out.println("Class name: " + this.getClass().getSimpleName());
+        System.out.println("toString: " + this.toString());
+        System.out.println("Equals: " + this.equals(anotherObject));
+    }
+}
